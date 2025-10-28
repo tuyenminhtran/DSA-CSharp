6688d463e5cd4d7c71b8f4a9e2afbdba497758df
Darling <tr.minhtuyen2006@gmail.com>
2025-10-28 08:21:49 +0700

    feat : update Node

---
+++ IntQueue/Node.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntQueue
+{
+    internal class Node
+    {
+        // attiributes
+        private int data;
+        private Node next;
+
+        // properties
+        public int Data { get => data; set => data = value; }
+        public Node Next { get => next; set => next = value; }
+
+        // constructor
+        public Node() { }
+        public Node( int x)
+        {
+            data = x;
+            next = null;
+        }
+    }
+}
